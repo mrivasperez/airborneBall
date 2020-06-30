acac03f371ffa1760a60995d8d6bc05d05b39866
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-06-29 21:13:57 -0700

    updates to script.js

--- script.js
+++ script.js
@@ -23,11 +23,19 @@ setInterval(() => {
 const jump = (e) => {
   jumping = 1;
   let jumpCount = 0;
-  const jumpIntervel = setInterval(() => {
-    jumpCount++;
-    character.style.top = `${characterTop - 5}px`;
-    if (jumpCount > 20) {
-      clearInterval(jumpIntervel);
+  const jumpInterval = setInterval(() => {
+    var characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue
+    );
+    if (character < 6 && jumpCount < 15) {
+      character.style.top = `${characterTop - 5}px`;
     }
+
+    if (jumpCount > 20) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
   }, 10);
 };
