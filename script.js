57461c4ac4f7011baf7b09a3e549785fc0ecdc69
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-06-29 22:09:01 -0700

    finished

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 var block = document.getElementById("block"),
   hole = document.getElementById("hole"),
   character = document.getElementById("character"),
-  jumping = 0;
+  jumping = 0,
+  counter = 0;
 
 hole.addEventListener("animationiteration", () => {
-  const random = -(Math.random() * 300 + 150);
-  hole.style.top = `${random}px`;
+  var random = Math.random() * 3,
+    top = -(random * 100 + 150);
+  hole.style.top = `${top}px`;
 });
 
 // gravity
 setInterval(() => {
   let characterTop = parseInt(
-    window.getComputedStyle(character).getPropertyValue("top")
-  );
+      window.getComputedStyle(character).getPropertyValue("top")
+    ),
+    blockLeft = parseInt(
+      window.getComputedStyle(block).getPropertyValue("left")
+    ),
+    holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top")),
+    cTop = -(500 - characterTop);
+
   if (jumping === 0) {
     character.style.top = `${characterTop + 3}px`;
   }
+
+  if (
+    characterTop > 480 ||
+    (blockLeft < 20 && blockLeft > -50 && cTop < holeTop) ||
+    cTop > holeTop + 130
+  ) {
+    alert(`Game over. Your score is ${counter}`);
+    character.style.top = `${100}px`;
+    counter = 0;
+  }
+  counter++;
 }, 10);
 
 // jumping
 
-const jump = (e) => {
+const jump = () => {
   jumping = 1;
   let jumpCount = 0;
-  const jumpInterval = setInterval(() => {
+  var jumpInterval = setInterval(() => {
     var characterTop = parseInt(
-      window.getComputedStyle(character).getPropertyValue
+      window.getComputedStyle(character).getPropertyValue("top")
     );
-    if (character < 6 && jumpCount < 15) {
+
+    if (characterTop > 6 && jumpCount < 15) {
       character.style.top = `${characterTop - 5}px`;
     }
 
@@ -36,6 +56,9 @@ const jump = (e) => {
       jumping = 0;
       jumpCount = 0;
     }
+
+    // if (){};
+
     jumpCount++;
   }, 10);
 };
